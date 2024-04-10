333f6c7b6a8f8a82ddb115e5072d7fe2195bf8de
Evgeni Agranat <eagranat@z2t7c4.42bangkok.com>
2024-04-10 14:27:34 +0700

    deleted commented functions

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: eagranat <eagranat@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 11:27:28 by eagranat          #+#    #+#             */
-/*   Updated: 2024/04/10 14:13:30 by eagranat         ###   ########.fr       */
+/*   Updated: 2024/04/10 14:21:29 by eagranat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,91 +73,6 @@ char	*find_path(char **envp, char *cmd)
 	free_split(paths);
 	return (cmd_path);
 }
-
-// int	out_process(int fds[2], char *outfile, char *cmd, char **envp)
-// {
-// 	int fd;
-// 	pid_t pid;
-// 	int execstat;
-// 	char *path;
-// 	int status;
-// 	char **cmd_split;
-
-// 	pid = fork();
-// 	if (pid == -1)
-// 		print_error("Fork error", 0);
-// 	else if (pid == 0)
-// 	{
-// 		close(fds[1]);
-// 		fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-// 		if (fd == -1)
-// 			print_error("Outfile error", 0);
-// 		dup2(fds[0], STDIN_FILENO);
-// 		dup2(fd, STDOUT_FILENO);
-// 		if (!*cmd)
-// 			print_error("Command not found", 0);
-// 		path = find_path(envp, cmd);
-// 		if (!path)
-// 			print_error("Command not found", 1);
-// 		cmd_split = ft_split(cmd, ' ');
-// 		execstat = execve(path, cmd_split, envp);
-// 		if (execstat == -1)
-// 		{
-// 			print_error("Execve error command failed / not found", 1);
-// 			free_split(cmd_split);
-// 		}
-// 		free_split(cmd_split);
-// 	}
-// 	else
-// 	{
-// 		close(fds[0]);
-// 		waitpid(pid, &status, 0);
-// 		if (WIFEXITED(status))
-// 			return (WEXITSTATUS(status));
-// 		else
-// 			return (EXIT_FAILURE);
-// 	}
-// 	return (EXIT_SUCCESS);
-// }
-
-// int	in_process(int fds[2], char *infile, char *cmd, char **envp)
-// {
-// 	int fd;
-// 	int pid;
-// 	int execstat;
-// 	int status;
-// 	char *path;
-
-// 	pid = fork();
-// 	if (pid == -1)
-// 		print_error("Fork error", 0);
-// 	else if (pid == 0)
-// 	{
-// 		close(fds[0]);
-// 		fd = open(infile, O_RDONLY);
-// 		if (fd == -1)
-// 			print_error("Infile error", 0);
-// 		dup2(fds[1], STDOUT_FILENO);
-// 		dup2(fd, STDIN_FILENO);
-// 		path = find_path(envp, cmd);
-// 		if (!path)
-// 			print_error("Command not found", 1);
-// 		execstat = execve(path, ft_split(cmd, ' '), envp);
-// 		if (execstat == -1)
-// 			print_error("Execve error command failed / not found", 1);
-// 	}
-// 	else
-// 	{
-// 		close(fds[1]);
-// 		waitpid(pid, &status, 0);
-// 		if (WIFEXITED(status))
-// 			return (WEXITSTATUS(status));
-// 		else
-// 			return (EXIT_FAILURE);
-
-// 	}
-// 	return (EXIT_SUCCESS);
-// }
 
 int	main(int argc, char **argv, char **envp)
 {
